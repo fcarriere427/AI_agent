dae76232015b37fbc21f41991eb0ef2b173ef109
florian <florian.carriere@gmail.com>
2024-09-07 21:26:03 +0200

    change docs directory to relative path

--- RAG_test.py
+++ RAG_test.py
@@ -2,6 +2,6 @@ from clarifai.rag import RAG
 
 rag_agent = RAG.setup(user_id="t8xj1dxh0def", llm_url="https://clarifai.com/anthropic/completion/models/claude-3_5-sonnet")
 
-rag_agent.upload(folder_path="/docs")
+rag_agent.upload(folder_path="./docs")
 
 rag_agent.chat(messages="Summarize this pdf")
