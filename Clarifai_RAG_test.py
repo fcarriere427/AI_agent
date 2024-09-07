fde06cecbb4687a381703233a0f720c5264a3bec
florian <florian.carriere@gmail.com>
2024-09-07 21:54:01 +0200

    add earth-search

--- Clarifai_RAG_test.py
+++ Clarifai_RAG_test.py
@@ -1,4 +1,4 @@
-from clarifai.rag import RAG
+from clarifai.rag import RAG # type: ignore
 
 rag_agent = RAG.setup(user_id="t8xj1dxh0def", llm_url="https://clarifai.com/anthropic/completion/models/claude-3_5-sonnet")
 
